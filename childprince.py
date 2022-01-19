71c34a664a5cfcf8af095a4b7daa2994097cf048
Kim KyungIm <97868363+kimkyung2@users.noreply.github.com>
2022-01-19 12:50:25 +0900

    Update childprince.py

--- childprince.py
+++ childprince.py
@@ -4,28 +4,36 @@ Created on Wed Jan 19 12:07:23 2022
 
 @author: -
 """
-
-import math
-
 Tcases = int(input())
 results = []
 
-for case in range(Tcases):
-    count = 0
-    x1, y1, x2, y2 = map(int,input().split())
-    pnumber = int(input())
-    planets = []
-    
-    for p in range(pnumber):
-        px, py, pr = map(int, input().split())
-        d1 = (x1-px)*(x1-px)**2 
-        d2 = (x2-px)*(x2-px)**2
-        
-        if d1 <pr**2 or d2<pr**2:
-            if d1 < pr**2 and d2 <pr**2:
-                pass
-            else:
-                count += 1
-print(count)
+def prince(Tcases):
 
+    for case in range(Tcases):
+        count = 0
+        x1, y1, x2, y2 = map(int,input().split())
+        pnumber = int(input())
+        planets = []
+        
+        for p in range(pnumber):
+            px, py, pr = map(int, input().split())
+            d1 = (x1-px)**2 + (y1-py)**2 
+            d2 = (x2-px)**2 + (y2-py)**2
+            
+            if d1 <pr**2 or d2<pr**2:
+                if d1 < pr**2 and d2 <pr**2:
+                    pass
+                else:
+                    count += 1  
+        results.append(count)
+
+def result(results):
+    for i in results:
+        print(i)
     
+    
+def test():
+    
+    prince(Tcases)
+    result(results)
+test()
